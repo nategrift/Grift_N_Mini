be10e17e67e64f18398151fc9e5a44226094521f
nategrift <nategrift@gmail.com>
2020-11-24 15:22:24 -0500

    Added car list and lightbox

--- js/main.js
+++ js/main.js
@@ -1,7 +1,44 @@
 // import your packages here
 import { fetchData } from "./modules/fetchData.js";
 
+(() => {
+    let mini_vue = new Vue({
+
+        data: {
+            selectedMiniVideo: '',
+            selectedCar: '',
+            mini_cars: [],
+            lightBoxShown: false,
+        },
+
+        // this is the "mounted" lifecycle hook. Vue is done creating itself, and has attached itself to the "app" div on the page
+        mounted: function() {
+            console.log("Vue is mounted!");
+
+            fetchData("./includes/index.php").then(data => this.mini_cars = data).catch(err => console.log(err));
+        },
 
 
+        methods: {
+            changeVideo(id) {
+                console.log("id");
+            },
+            updateCars(cars) {
+                console.log('Updated cars');
+                console.log(cars);
+                this.mini_cars = cars;
+            },
+            carLightBox(carID) {
+                fetchData(`./includes/index.php?id=${carID}`).then(data => this.showLightBox(data[0])).catch(err => console.log(err));
+            },
+            showLightBox(car) {
+                this.selectedCar = car;
+                this.lightBoxShown = true;
+            },
+            hideLightBox() {
+                this.lightBoxShown = false;
+            }
+        }
+    }).$mount("#app"); 
 
-fetchData("./includes/index.php").then(data => console.log(data)).catch(err => console.log(err));
+})();
